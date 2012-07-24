81e377a168b931e46399be26b3e2327a0cda6e57
slz <slz@github>
2012-07-24 00:08:11 -0700

    governors: preparing to add interactive_slz governor. the interactive code is unmodified at this point.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -373,6 +373,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE_SLZ)
+extern struct cpufreq_governor cpufreq_gov_interactive_slz;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_interactive_slz)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
